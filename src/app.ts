9c36518f1dce14cbb785d3802a1d6ec45a975745
yanis <yanis.hammada@ricardo.ch>
2019-11-25 17:15:39 +0100

    JSON middleware

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import express, { Request, Response } from 'express'
 
 const app: express.Application = express()
 
+app.use(express.json())
+
 app.get('/', (_req: Request, res: Response) => {
-  console.log('fu')
   res.status(200).send()
 })
 
